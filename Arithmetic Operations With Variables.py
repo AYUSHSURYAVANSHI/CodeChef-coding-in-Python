763541000011b22b88c6b06ad1ad1bcf80f27f50
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-16 23:03:22 +0530

    Create Arithmetic Operations With Variables.py

---
+++ Arithmetic Operations With Variables.py
@@ -0,0 +1,16 @@
+#Solution as follows
+s = 10
+areas = s*s
+
+l = 25
+b = 17
+arear = l*b
+
+#In this example, area of rectangle is higher than the area of the square
+areadiff = arear - areas
+costs = areas * 4
+
+print("The area of Square is", areas )
+print("The area of Rectangle is",  arear )
+print("The difference of areas is", areadiff)
+print("Cost of painting the Square is", costs)
