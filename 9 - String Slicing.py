73758b586a56f32cc2e0f75a205152d5aaa0baff
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-17 23:11:59 +0530

    Create 9 - String Slicing.py

---
+++ 9 - String Slicing.py
@@ -0,0 +1,13 @@
+b = "Python is fun"
+
+#Expected output 'Pytho'
+print(b[:5]) 
+
+#Expected output 'thon is fun'
+print(b[2:])
+
+#Expected output 'hon'
+print(b[3:6])
+
+##Expected output 'Py'
+print(b[0:2])
