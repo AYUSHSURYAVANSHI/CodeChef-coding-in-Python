2e7e9e17476a6826bf508b255458352302123a76
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-17 23:13:28 +0530

    Create 11 - Modifying Strings.py

---
+++ 11 - Modifying Strings.py
@@ -0,0 +1,7 @@
+a = "Bootstrap"
+b = "TYPESCRIPT"
+print( a.upper()) 
+print( b.lower() ) 
+c = "Welcome to ChefCode! You can learn Python with ChefCode"
+d = c.replace("ChefCode", "CodeChef")
+print(d)
