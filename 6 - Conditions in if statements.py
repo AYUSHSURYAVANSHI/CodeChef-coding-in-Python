b75ee51ed496418ac5411fae52c5b3a8157e98a8
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:11:25 +0530

    Create 6 - Conditions in if statements.py

---
+++ 6 - Conditions in if statements.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a >= c: 
+  print("Bravo!")
+else:
+  print("Try Again")
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a >= c: 
+  print("Bravo!")
+else:
+  print("Try Again")
