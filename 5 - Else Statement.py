36ee99b717cc0e81c0688a2aa7e7ee7dedf8fbb7
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:10:55 +0530

    Create 5 - Else Statement.py

---
+++ 5 - Else Statement.py
@@ -0,0 +1,26 @@
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
+
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
+
+r = int(input())
+k = int(input())
+if r > k:
+  print("Ram is heavier than Karan")
+elif k >r:
+  print("Karan is heavier than Ram")
+else:
+  print("Ram & Karan have the same weight!")
