1930aad86648185fa58bb6466816c87cbd1dc402
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:07:44 +0530

    Create 1 - If and else Statement.py

---
+++ 1 - If and else Statement.py
@@ -0,0 +1,20 @@
+r = 1002
+w = 3222
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
+
+r = 789
+w = 3222
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
+
+r = 300
+w = 233
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
