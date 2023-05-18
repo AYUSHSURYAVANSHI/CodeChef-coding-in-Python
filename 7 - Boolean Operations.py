f44ac56c7465d41e4cc569b577d86dc86c2b94a3
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:12:04 +0530

    Create 7 - Boolean Operations.py

---
+++ 7 - Boolean Operations.py
@@ -0,0 +1,12 @@
+a = 100
+b = 200
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+c = 150
+x = 144
+if c > x:
+  print("c is greater than x")
+else :
+  print("c is not greater than x")
