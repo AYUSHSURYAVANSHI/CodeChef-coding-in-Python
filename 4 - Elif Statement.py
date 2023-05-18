58d21776fc17a67c84282dd0c635aac87cf0b303
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:10:25 +0530

    Create 4 - Elif Statement.py

---
+++ 4 - Elif Statement.py
@@ -0,0 +1,13 @@
+b = 23
+r = 45
+if r > b:
+  print("Rob Scored higher marks than Bob")
+elif b == r:
+  print("Bob & Rob both scored the same")
+  
+b = 15
+r = 15
+if r > b:
+  print("Rob Scored higher marks than Bob")
+elif b == r:
+  print("Bob & Rob both scored the same")
