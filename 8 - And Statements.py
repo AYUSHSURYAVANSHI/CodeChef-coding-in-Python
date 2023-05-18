a431406137c8e45629aff719a780a4fd00731dcd
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:12:39 +0530

    Create 8 - And Statements.py

---
+++ 8 - And Statements.py
@@ -0,0 +1,11 @@
+a = 15
+if (a%7 == 0) and (a%5 == 0): #%is the sign of modulus
+  print("The number is divisible by both 5 & 7")
+else:
+  print("The number is not divisible by both 5 & 7")
+
+b = 35
+if (b%7 == 0) and (b%5 == 0): #%is the sign of modulus
+  print("The number is divisible by both 5 & 7")
+else:
+  print("The number is not divisible by both 5 & 7")
