917785589d2d3b89c7494a350c411e6270bc30fa
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:14:01 +0530

    Create 10 - Fix the Code.py

---
+++ 10 - Fix the Code.py
@@ -0,0 +1,12 @@
+#Solution as follows
+#Indentation is important in lines 8, 10, 12
+# ':' is important in line 9 and 11
+
+r = 206
+v = 122
+if v > r:
+    print("v is greater than r")
+elif r == v:
+    print("v and r are equal")
+else:
+    print("v is less than r")
