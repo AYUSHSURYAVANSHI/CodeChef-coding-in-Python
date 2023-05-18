e3190700a557d3ed2bb460f81295e45a098e4929
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:14:39 +0530

    Create 11 - Problems on Condition statements.py

---
+++ 11 - Problems on Condition statements.py
@@ -0,0 +1,13 @@
+x =int(input())
+if x > 70:
+   print ("Order Limit reached")
+else:
+   print("Order Confirmed")
+print("Thank YOU!")
+
+x =int(input())
+if x > 70:
+   print ("Order Limit reached")
+else:
+   print("Order Confirmed")
+print("Thank YOU!")
