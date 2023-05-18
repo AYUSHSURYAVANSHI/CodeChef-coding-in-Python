874155bc507fd77a0d22fe82fad1dd770cf04247
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-18 22:13:21 +0530

    Create 9 - Or Statement.py

---
+++ 9 - Or Statement.py
@@ -0,0 +1,15 @@
+z = int(input())
+x = int(input())
+c = int(input())
+if c > x or c > z:
+  print("PASS")
+else:
+  print("FAIL")
+
+z = int(input())
+x = int(input())
+c = int(input())
+if c > x or c > z:
+  print("PASS")
+else:
+  print("FAIL")
